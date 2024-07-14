b194dee9874ca923333a6dbf38b31d99fd7cfd7c
Piyush Varman (PCV) <varmanpiyush@gmail.com>
2024-07-14 21:40:06 +0530

    Create Learnings.cpp

---
+++ Learnings.cpp
@@ -0,0 +1,12 @@
+/* :: - Scope Resolution Operator
+// used for single line comment, /* used for starting a multi-line comment
+using namespace <namespace_name> // to help reduce the number of times we use the namespace name in our program
+
+Program should have:
+
+#include <iostream>
+using namespace <name>; // optional
+int main () {
+    *** code ***
+    return 0; // to successfully terminate a program by returning the value 0
+*/
