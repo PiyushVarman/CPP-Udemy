1712f346c7de3796780558e7115b3880d1a2e32a
Piyush Varman (PCV) <varmanpiyush@gmail.com>
2024-07-14 21:28:01 +0530

    Add files via upload

---
+++ Section_Challenge_1/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+/* Hello
+ * world*/
+int main() {
+    int favenum;
+    std::cout << "Enter your favourite number between 1 and 100:";
+    std::cin >> favenum;
+    std::cout << "Amazing! That's my favourite number too!" << std::endl;
+    std::cout << "No really!!, " << favenum << " is my favourite number!" << std::endl;
+    return 0;
+}
+
